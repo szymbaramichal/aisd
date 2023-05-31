e00021c4c4f9f58694ff8d5ba30ad8d2004eed2d
szymbaramichal <szymbaramichal@gmail.com>
2023-05-31 16:01:43 +0200

    Sorts fix

--- sorts.c
+++ sorts.c
@@ -37,26 +37,13 @@ int main() {
 }
 
 // https://youtu.be/LeWuki7AQLo
-// Performs a merge sort of the array a with the given length, the function 
-// provides an easier/prettier interface for using merge sort for the 
-// programmer as they simply need to provide the array and its length
 void merge_sort(int a[], int length)
 {
-  // call the merge sort recursion function, the left index of 0 and the
-  // right index of length - 1 are provided as we are initially looking 
-  // at sorting "the entire array"
   merge_sort_recursion(a, 0, length - 1);
 }
 
-// Applies the merge sort algorithm to the array a between the left index l
-// and the right index r.  This function implements the recursive 
-// divide-and-conquer step of the merge sort algorithm, splitting the array
-// portion between l...r at the middle, and calling itself on each portion, 
-// before applying the function to merge the sorted portions of the array 
-// that will result.
 void merge_sort_recursion(int a[], int l, int r)
 {
-  // we stop recursion when l >= r
   if (l < r)
   {
     // find the midpoint of l and r
@@ -73,36 +60,25 @@ void merge_sort_recursion(int a[], int l, int r)
   }
 }
 
-// merges the two sorted portions of the array a between the indexes l ... m
-// and m + 1 ... r
+//merges the two sorted portions of the array a between the indexes l ... m
+//and m + 1 ... r
 void merge_sorted_arrays(int a[], int l, int m, int r)
 {
-  // calculate the length of the left and right portions of the array
   int left_length = m - l + 1;
   int right_length = r - m;
   
-  // create temporary arrays to store these portions
   int temp_left[left_length];
   int temp_right[right_length];
   
-  // used as index/counter variables for the 3 arrays a, temp_left, temp_right
   int i, j, k;
   
-  // copy the left portion into the temp_left array
   for (int i = 0; i < left_length; i++)
     temp_left[i] = a[l + i];
   
-  // copy the right portion into the temp_right array
   for (int i = 0; i < right_length; i++)
     temp_right[i] = a[m + 1 + i];
   
-  // Use i to move through the indexes of temp_left, j to move through the 
-  // indexes of temp_right, and k to move through the portion of the array 
-  // a from l ... r.  We basically keep checking the "head" of temp_left 
-  // and temp_right (knowing both arrays are sorted) and put the smaller of 
-  // the two into array a (using i, j, k to move through the arrays).  When
-  // we run out elements in either temp_left or temp_right, the remaining 
-  // elements from the other array will be copied over into a.
+
   for (i = 0, j = 0, k = l; k <= r; k++)
   {
     // so long as we have not already reached the end of the temp_left array 
@@ -170,7 +146,6 @@ void bubble_sort(int *tab, int size) {
                 int temp = tab[j - 1];
                 tab[j - 1] = tab[j];
                 tab[j] = temp;
-                j++;
             }
         }
     }
